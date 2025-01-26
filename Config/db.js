1b4f70f9e569bd2e8c80a32d5571a908c34ee6bf
quotely-co <quotely7@gmail.com>
2025-01-27 01:18:28 +0530

    update th backend URL

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect("mongodb+srv://shamilamiyan:303358020@cluster0.wuzzzv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/factory", factoryroutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/products", productRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
