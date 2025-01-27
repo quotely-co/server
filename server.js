8fa312940c14caf12990882066c9bdf2720ecb25
quotely-co <quotely7@gmail.com>
2025-01-27 19:31:07 +0530

    updated server 4

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Connect to MongoDB
 connectDB();  // Ensure this is called after .env is loaded
 
-// Middleware
+// Middleware 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
