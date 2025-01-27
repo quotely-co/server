a18c32b519a82b1d788b7e1181e60ee80079f1d6
quotely-co <quotely7@gmail.com>
2025-01-27 19:39:26 +0530

    remove:removed the route

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Simple Test Route
+// Simple Test Routes
 app.get("/", (req, res) => {
   res.send("Hello, server is running!");
 });
