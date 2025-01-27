a45bec6b38b558dccdf7777fb8804816ab810fb0
quotely-co <quotely7@gmail.com>
2025-01-27 18:43:45 +0530

    updated server

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const userRoute = require("./routes/User");
 const paymentRoutes = require("./routes/Payment");
 const productRoute = require("./routes/product");
 const factoryroutes = require("./routes/factory");
-
+// Port configuration
+const port = process.env.PORT || 4000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 
 // Connect to the database
 connectDB();
@@ -26,17 +32,9 @@ app.use("/api/factory", factoryroutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/products", productRoute);
 
-// Deployment setup
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
 
-// Port configuration
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
