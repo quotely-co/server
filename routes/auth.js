5531b28690c0438aa1c63cb75fc7af0a02e59fd2
shaamiilll <shamilamiyan@gmail.com>
2026-01-25 23:03:39 +0530

    Fix formatting and update Google callback URI in auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,22 +2,22 @@ const express = require("express");
 const axios = require("axios");
 const querystring = require("querystring");
 const jwt = require('jsonwebtoken');
-const { registerUser, loginUser, verifyToken,verifyFactoryOTP, verifyOTP ,sendOtp, google, googleCallback, check_factory ,logout} = require("../controllers/authController");
+const { registerUser, loginUser, verifyToken, verifyFactoryOTP, verifyOTP, sendOtp, google, googleCallback, check_factory, logout } = require("../controllers/authController");
 const User = require("../models/User");
 
 const router = express.Router();
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const REDIRECT_URI = "https://api.quotely.shop/api/auth/google/callback";
+const REDIRECT_URI = "https://server-bshw.onrender.com/api/auth/google/callback";
 
 // auth for factory
 // router.post("/register", registerUser); // register facory is done in factory.js
 router.post("/verify-otp", verifyOTP);
 router.post("/factory/login", loginUser);
 router.post("/check_factory", check_factory);
-router.post('/send-otp' , sendOtp)
+router.post('/send-otp', sendOtp)
 router.post("/factory/verify-otp", verifyFactoryOTP);
-router.post('/logout' , logout)
+router.post('/logout', logout)
 
 // google auth and user creation
 router.get("/google", google)
